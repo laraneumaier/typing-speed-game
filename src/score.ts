0a2e7d1990f53d7418ca06e49172c7b5c22b9a3c
laraneumaier <neumaier.lara@web.de>
2020-04-20 15:11:04 +0200

    local storage added

--- src/score.ts
+++ src/score.ts
@@ -8,7 +8,7 @@ let highScore:number = 0;
 export function startScore (){
     scoreInput.classList.add("score");
     score = 0;
-    scoreInput.innerHTML = "";
+    scoreInput.innerHTML = "Score: " + score;
 }
 export function plusScore(n:number){
     showScore(score +=n);
@@ -17,37 +17,23 @@ export function showScore (n:number){
     scoreInput.innerHTML = "Score: " + String(score);
     return score;
 }
-/*
-export function showHighScore (){
-    if ( score > highScore || highScore === 0){
-        highScoreInput.innerHTML = "Highscore: " + String(score);
-        highScore = score;
-    } else{
-        highScoreInput.innerHTML = "Highscore: " + String (highScore);
-    }
-    scoreInput.appendChild(highScoreInput);
-    scoreInput.classList.remove("score");
-    return highScore;
-}
-*/
+
 export function showHighScore(){
     if(localStorage.getItem("Highscore")){ 
-        if(score > parseInt(localStorage.getItem("Highscore")!) || parseInt(localStorage.getItem("Highscore")!) === 0){ //will never be null
-            highScoreInput.innerHTML = "Highscore: " + String(score);
+        if(score > parseInt(localStorage.getItem("Highscore")!)){ //will never be null
             highScore = score;
+            highScoreInput.innerHTML = "Highscore: " + String(highScore);
             localStorage.setItem("Highscore", highScore.toString());
         } else{
             highScoreInput.innerHTML = "Highscore: " + localStorage.getItem("Highscore");
         }
     } else{
         highScore = score;
-        highScoreInput.innerHTML = "Highscore: " + String(score);
+        highScoreInput.innerHTML = "Highscore: " + String(highScore);
         localStorage.setItem("Highscore", highScore.toString());
-
     }
     scoreInput.appendChild(highScoreInput);
     scoreInput.classList.remove("score");
-    return parseInt(localStorage.getItem("Highscore")!);
-    
+    return parseInt(localStorage.getItem("Highscore")!);  
 }
 
